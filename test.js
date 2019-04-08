cd82b1cbef35e826b4f4e3ce54d80cbd8b9f76e7
hOch1 <49400239+hOch1@users.noreply.github.com>
2019-04-08 23:14:23 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+document.write("파일형태로 만든 자바스크립트 내용입니다.<br>");
+document.write("HTML문서에서 읽어 처리할 수 있습니다.<br>");
+
+document.write("모니터 해상도"+"<p>");
+document.write("높이:"+screen.height+"<p>");
+document.write("넓이:"+screen.width+"<p>");
---
+++ window-alert.js
@@ -0,0 +1,5 @@
+<script>
+  function alerDialogBox() {
+    alert("확인은 누를 때까지 다른작업을 할 수 없어요")
+  }
+</script>
